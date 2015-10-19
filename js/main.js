24c43fdd1ef58e2f0ccf7fe09f3a97c1fda8ec04
Chris Antaki <ChrisAntaki@gmail.com>
2015-10-19 15:19:54 -0700

    Cleanup

--- js/main.js
+++ js/main.js
@@ -218,33 +218,37 @@ function showThanks() {
 
 
 // Scroll to anchors
-var links = document.querySelectorAll(".animated-scroll")
-var i = links.length
-var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
+var links = document.querySelectorAll(".animated-scroll");
+var i = links.length;
+var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body;
 var easeInOutCubic = function(t, b, c, d) {
-if ((t/=d/2) < 1) return c/2*t*t*t + b
-return c/2*((t-=2)*t*t + 2) + b
+    if ((t /= d / 2) < 1) {
+        return c / 2 * t * t * t + b;
+    }
+
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
 }
 
-while (i--)
-links.item(i).addEventListener("click", function(e) {
-  var startTime
-  var startPos = root.scrollTop
-  var targetId = this.getAttribute('data-target')
-  var endPos = document.getElementById(targetId).getBoundingClientRect().top
-  var maxScroll = root.scrollHeight - window.innerHeight
-  var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
-  var duration = 900
-  var scroll = function(timestamp) {
-    startTime = startTime || timestamp
-    var elapsed = timestamp - startTime
-    var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration)
-    root.scrollTop = progress
-    elapsed < duration && requestAnimationFrame(scroll)
-  }
-  requestAnimationFrame(scroll)
-  e.preventDefault()
-})
+while (i--) {
+    links.item(i).addEventListener("click", function(e) {
+        var startTime;
+        var startPos = root.scrollTop;
+        var targetId = this.getAttribute('data-target');
+        var endPos = document.getElementById(targetId).getBoundingClientRect().top;
+        var maxScroll = root.scrollHeight - window.innerHeight;
+        var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos;
+        var duration = 900;
+        var scroll = function(timestamp) {
+            startTime = startTime || timestamp;
+            var elapsed = timestamp - startTime;
+            var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration);
+            root.scrollTop = progress;
+            elapsed < duration && requestAnimationFrame(scroll);
+        }
+        requestAnimationFrame(scroll);
+        e.preventDefault();
+    });
+}
 
 
 
