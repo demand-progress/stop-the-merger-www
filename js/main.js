8a1eba039d51ca74c27ff69f98c7b98ac5e57992
Chris Antaki <ChrisAntaki@gmail.com>
2015-10-19 15:12:39 -0700

    Added links and scroll animation

--- js/main.js
+++ js/main.js
@@ -165,11 +165,6 @@ for (var i = 0; i < ems.length; i++) {
     }, false);
 }
 
-document.querySelector('button.add-your-name').addEventListener('click', function(e) {
-    e.preventDefault();
-    location.hash = 'add-your-name';
-});
-
 document.querySelector('a.the-letter').addEventListener('click', function(e) {
     e.preventDefault();
     modal_show('letter');
@@ -219,6 +214,37 @@ function showThanks() {
     thanks.style.display = 'block';
     thanks.style.opacity = 1;
 }
+
+
+
+// Scroll to anchors
+var links = document.querySelectorAll(".animated-scroll")
+var i = links.length
+var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
+var easeInOutCubic = function(t, b, c, d) {
+if ((t/=d/2) < 1) return c/2*t*t*t + b
+return c/2*((t-=2)*t*t + 2) + b
+}
+
+while (i--)
+links.item(i).addEventListener("click", function(e) {
+  var startTime
+  var startPos = root.scrollTop
+  var targetId = this.getAttribute('data-target')
+  var endPos = document.getElementById(targetId).getBoundingClientRect().top
+  var maxScroll = root.scrollHeight - window.innerHeight
+  var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
+  var duration = 900
+  var scroll = function(timestamp) {
+    startTime = startTime || timestamp
+    var elapsed = timestamp - startTime
+    var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration)
+    root.scrollTop = progress
+    elapsed < duration && requestAnimationFrame(scroll)
+  }
+  requestAnimationFrame(scroll)
+  e.preventDefault()
+})
 
 
 
